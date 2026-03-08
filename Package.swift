5ea3efa056c74371310ac81a11ffad220bad0ed0
Srihan Yeleswarapu <srihan.yeleswarapu@gmail.com>
2026-03-07 23:16:01 -0700

    Fix: Add resolvePackageDependencies to build workflow and standardize Firebase naming

--- Package.swift
+++ Package.swift
@@ -10,14 +10,14 @@ let package = Package(
         .library(name: "SmartSpeedCompanion", targets: ["SmartSpeedCompanion"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0")
+        .package(name: "Firebase", url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.0.0")
     ],
     targets: [
         .target(
             name: "SmartSpeedCompanion",
             dependencies: [
-                .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseDatabase", package: "firebase-ios-sdk")
+                .product(name: "FirebaseCore", package: "Firebase"),
+                .product(name: "FirebaseDatabase", package: "Firebase")
             ],
             path: "SmartSpeedCompanion",
             exclude: ["Info.plist", "Resources/Entitlements/SmartSpeedCompanion.entitlements"]
