467c78d1e7e5b34db07316a6b29169b10aabf8a3
Srihan Yeleswarapu <srihan.yeleswarapu@gmail.com>
2026-03-08 20:07:37 -0700

    Fixed Dependencies in Package.swift for FirebaseCore

--- Package.swift
+++ Package.swift
@@ -6,7 +6,26 @@ let package = Package(
     platforms: [
         .iOS(.v17)
     ],
-    products: [],
-    dependencies: [],
-    targets: []
+    products: [
+        .library(
+            name: "SmartSpeedCompanion",
+            targets: ["SmartSpeedCompanion"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.5.0")
+    ],
+    targets: [
+        .target(
+            name: "SmartSpeedCompanion",
+            dependencies: [
+                .product(name: "FirebaseCore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseDatabase", package: "firebase-ios-sdk")
+            ],
+            path: "SmartSpeedCompanion",
+            resources: [
+                .process("Resources")
+            ]
+        )
+    ]
 )
